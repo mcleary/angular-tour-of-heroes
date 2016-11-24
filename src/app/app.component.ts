5c9dfc12e17dd8ef380e08c4d354d5ef3ad358c7
Thales Sabino <tluisrs@gmail.com>
2016-11-24 20:00:56 -0200

    hero-detail component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,18 +20,15 @@ const HEROES: Hero[] = [
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>{{ title }}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-      <li *ngFor="let hero of heroes">
-        <span class="badge">{{ hero.id }}</span> {{ hero.name }}
-      </li>
-    </ul>
-  `,
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero) : void {
+    this.selectedHero = hero;
+  }
 }
