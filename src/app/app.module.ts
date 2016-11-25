7e4fbf2f1dcaf9ae8126becffc42d182ca9e9483
Thales Sabino <tluisrs@gmail.com>
2016-11-25 17:23:39 -0200

    More styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component'
@@ -10,30 +8,13 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from "./hero.service";
 import { DashboardComponent } from './dashboard.component';
 
+import { AppRoutingModule } from "./app-routing.module";
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    RouterModule.forRoot([
-      {
-        path: "",
-        redirectTo: "/dashboard",
-        pathMatch: "full"
-      },
-      {
-        path: "dashboard",
-        component: DashboardComponent
-      },
-      {
-        path: "heroes",
-        component: HeroesComponent
-      },
-      {
-        path: "detail/:id",
-        component: HeroDetailComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -7,13 +8,16 @@ import { HeroService } from './hero.service';
 @Component({
   selector: 'my-heroes',
   templateUrl: 'heroes.component.html',
-  styleUrls: ['heroes.component.css']
+  styleUrls: [ 'heroes.component.css' ]
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService : HeroService) {
+  constructor(
+    private heroService : HeroService,
+    private router : Router
+  ) {
 
   }
 
@@ -28,5 +32,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero) : void {
     this.selectedHero = hero;
+  }
+
+  gotoDetails(): void {
+    this.router.navigate(['/detail', this.selectedHero.id])
   }
 }
