54a9729470288d942eb1f490a22a646064b794d3
Thales Sabino <tluisrs@gmail.com>
2016-11-28 10:52:17 -0200

    Add and Delete Heroes funcionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -31,6 +31,23 @@ export class HeroService {
       .then(() => this.getHeroes());
   }
 
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+      .toPromise()
+      .then(res => res.json().data)
+      .catch(this.handleError);
+  }
+
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http
+      .delete(url, { headers: this.headers })
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
   update(hero : Hero) : Promise<Hero> {
     const url = `${this.heroesUrl}/${hero.id}`;
     return this.http
